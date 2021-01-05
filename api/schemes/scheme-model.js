be9972d153c1ec7c9ec09e7d96a7ab6080f10bae
Tucker <leachtucker@gmail.com>
2021-01-04 19:36:10 -0800

    Built three helper DB functions for Schemes

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -3,11 +3,34 @@ const db = require('../../data/db-config');
 
 // Helper functions
 function find() {
-    db('')
+    return db('schemes');
+}
+
+function findById(id) {
+    let schemaObject = db('schemes').where({ id: id })[0];
+    if (!schemaObject) {
+        return Promise.resolve(null);
+    }
+
+    return schemaObject;
+}
+
+function findSteps(id) {
+    const stepsArray = db('steps as s')
+        .join('schemes as sch', 's.scheme_id', 'sch.id')
+        .select('s.id', 'sch.scheme_name', 's.step_number', 's.instructions')
+        .where('sch.id', id);
+
+    if (!stepsArray) {
+        return Promise.resolve(null);
+    }
+
+    return stepsArray;
 }
 
 // Export
 module.exports = {
-    find
-
+    find,
+    findById,
+    findSteps
 };
